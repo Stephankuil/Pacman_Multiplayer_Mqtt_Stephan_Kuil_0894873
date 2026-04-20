1e452c37d9235db38fe8a633dc5f2072dbb6c094
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-04-20 14:31:51 +0200

    circle kan links en rechts bewegen.

--- practice_mqtt_connection/multi_circle.py
+++ practice_mqtt_connection/multi_circle.py
@@ -10,12 +10,22 @@ circle_y_coordinate = 400
 circle_size = 50
 screen = pygame.display.set_mode((width, height))
 
+
 running = True
 
 while running:
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    if keys[pygame.K_LEFT]:
+        circle_x_coordinate -= 10
+    if keys[pygame.K_RIGHT]:
+        circle_x_coordinate += 10
+
 
     screen.fill((255, 255, 255))
 
