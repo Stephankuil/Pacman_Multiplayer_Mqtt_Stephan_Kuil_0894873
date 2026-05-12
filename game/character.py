a6643c785d2d8393c10aecd33b914fac14aacba4
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-05-12 14:35:48 +0200

    i made the charakter.py file so the tests are green.

---
+++ game/character.py
@@ -0,0 +1,35 @@
+from game.gameobject import GameObject
+class Character(GameObject):
+    def __init__(self, name, score, x_coordinate, y_coordinate):
+        super().__init__(x_coordinate, y_coordinate)
+        self.name = name
+        self.score = score
+
+    def move(self, direction, level_map):
+        if direction == "LEFT":
+            new_x = self.x_coordinate - 1
+            new_y = self.y_coordinate
+
+        elif direction == "RIGHT":
+            new_x = self.x_coordinate + 1
+            new_y = self.y_coordinate
+
+        elif direction == "UP":
+            new_x = self.x_coordinate
+            new_y = self.y_coordinate - 1
+
+        elif direction == "DOWN":
+            new_x = self.x_coordinate
+            new_y = self.y_coordinate + 1
+
+        else:
+            return
+
+        if not level_map.is_wall(new_x, new_y):
+            self.x_coordinate = new_x
+            self.y_coordinate = new_y
+
+    def add_score(self, points):
+        self.score += points
+
+
