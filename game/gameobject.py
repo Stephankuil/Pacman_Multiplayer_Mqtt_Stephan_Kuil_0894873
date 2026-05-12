0d936066fa1bb84df803dec72b0e786901b42a28
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-05-12 14:55:14 +0200

    bugfix: i added images to gameobject so i needed to make changes to character

--- game/gameobject.py
+++ game/gameobject.py
@@ -1,6 +1,10 @@
 
 
 class GameObject:
-    def __init__(self, xcoordinate, ycoordinate):
+    def __init__(self, xcoordinate, ycoordinate, image):
         self.x_coordinate = xcoordinate
         self.y_coordinate = ycoordinate
+        self.image = image
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
