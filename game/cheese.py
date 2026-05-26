63eb405d7a492a10d9b3a23f2a5d47ad35fb4cef
Stephan_kuil_0894873 <127349839+HappyPancake098@users.noreply.github.com>
2026-05-26 14:01:17 +0200

    creating the cheese class

--- game/cheese.py
+++ game/cheese.py
@@ -1,8 +1,23 @@
+from game.item import Item
 
-class Cheese():
+class Cheese(Item):
     def __init__(self, x_coordinate, y_coordinate):
+        super().__init__()
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         image = None
         rgb_color = (255, 255, 0)
+
+        cheese_list = []
+
+    def how_many_left(self, cheese_list):
+        amount_left = 0
+
+        for cheese in cheese_list:
+            if not cheese.consumed:
+                amount_left += 1
+
+        return amount_left
+
+
 
